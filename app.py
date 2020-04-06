d3e1bed02bdf524c5c17978e71d559593ea25397
Djang <django.heimgartner@gmail.com>
2020-04-06 21:33:08 +0200

    Created an edit function to be able to edit created transactions

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def get_dashboard():
 @app.route('/get_transactions')
 def get_transactions():
     return render_template('transactions.html',
-                           transactions = mongo.db.transactions.find(),
+                           transactions=mongo.db.transactions.find(),
                            categories=mongo.db.categories.find())
-                           #transactions = mongo.db.transactions.find({"catagory":"Household"}))
+                           # transactions = mongo.db.transactions.find({"catagory":"Household"}))
 
 
 """Creates a transaction to the database after the button is pressed"""
@@ -39,6 +39,28 @@ def insert_transaction():
     return redirect(url_for('get_transactions'))
 
 
+@app.route('/edit_transaction/<transaction_id>')
+def edit_transaction(transaction_id):
+    the_transaction = mongo.db.transactions.find_one({"_id": ObjectId(transaction_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittransaction.html', transaction=the_transaction, categories=all_categories)
+
+
+@app.route('/update_transaction/<transaction_id>', methods=['POST'])
+def update_transaction(transaction_id):
+    transactions = mongo.db.transactions
+    transactions.update({'_id': ObjectId(transaction_id)},
+                 {
+        'transition': request.form.get('transition'),
+        'category_name': request.form.get('category_name'),
+        'details': request.form.get('details'),
+        'date': request.form.get('date'),
+        'amount': request.form.get('amount')
+    })
+    return redirect(url_for('get_transactions'))
+
+
+"""Deletes a transaction by searching on it's id"""
 @app.route('/delete_transaction/<transaction_id>')
 def delete_transaction(transaction_id):
     mongo.db.transactions.remove({'_id': ObjectId(transaction_id)})
