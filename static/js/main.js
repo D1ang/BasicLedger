9934389ffec5800d3872e308adb5f3200f6046aa
Djang <django.heimgartner@gmail.com>
2020-04-18 15:57:52 +0200

    Placed all the Javascript in main.js and created an edit transaction modal function

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,22 @@
 /*------------------------JQuery function to check the amount field for two decimals------------------------*/
-
 $('.two-decimals').change(function() {
 	this.value = parseFloat(this.value).toFixed(2);
+});
+
+/*----------------------Edit transaction modal and fill the formfields with Json data-----------------------*/
+function editTransaction(id) {
+	$.ajax({
+		url: `/edit_transaction/${id}`
+	}).done(function(response) {
+		console.log(response);
+		let updateURL = `/update_transaction/${id}`;
+		$('#editForm').attr('action', updateURL);
+		$('#EditTransactionModal').modal('show');
+	});
+}
+
+/*---------------------------Loading the libraries for the datepicker & DataTable---------------------------*/
+$(document).ready(function() {
+	var datepicker = flatpickr('#date', { dateFormat: 'j F, Y', inline: true });
+	$('#table_id').DataTable({ lengthChange: false, responsive: true });
 });
