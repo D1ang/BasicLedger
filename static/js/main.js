7ed07b7b48c70fd8d2e0f3bf4135842336dc6872
Djang <django.heimgartner@gmail.com>
2020-04-19 22:51:10 +0200

    Added options to the datatable to improve the functionality

--- static/js/main.js
+++ static/js/main.js
@@ -4,24 +4,41 @@ function editTransaction(id) {
 	$.ajax({
 		url: `/edit_transaction/${id}`
 	}).done(function(response) {
+    
+    let data = JSON.parse(response);
 
-    let data = JSON.parse(response)
+		document.getElementById('demo1').value = data.transition;
+		document.getElementById('editCategory').value = data.category_name;
+		document.getElementById('editDescription').value = data.details;
+		document.getElementById('editDate').value = data.date;
+		document.getElementById('editAmount').value = data.amount;
 
-    document.getElementById('demo1').value = data.transition;
-    document.getElementById('editCategory').value = data.category_name;
-    document.getElementById('editDescription').value = data.details;
-    document.getElementById('editDate').value = data.date;
-    document.getElementById('editAmount').value = data.amount;
-
-    let updateURL = `/update_transaction/${id}`;
+		let updateURL = `/update_transaction/${id}`;
 		$('#editForm').attr('action', updateURL);
 		$('#EditTransactionModal').modal('show');
-  });
+	});
 }
 
 /*---------------------------Loading the libraries for the datepicker & DataTable---------------------------*/
 
 $(document).ready(function() {
 	var datepicker = flatpickr('#date', { dateFormat: 'j F, Y', inline: true });
-	$('#table_id').DataTable({ lengthChange: false, responsive: true });
+
+	var table = $('#transactionTable').DataTable({
+		lengthChange: false,
+		dom: 'lrtip',
+		responsive: true,
+		columns: [
+			{ responsivePriority: 1 },
+			{ responsivePriority: 5 },
+			{ responsivePriority: 4 },
+			{ responsivePriority: 2 },
+			{ responsivePriority: 3 },
+			{ responsivePriority: 6 }
+		]
+	});
+
+	$('#dataTableSearch').on('keyup', function() {
+		table.search(this.value).draw();
+	});
 });
