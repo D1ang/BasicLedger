62e820c06c3fcd80fead536e5fda55ddffed6c71
Djang <django.heimgartner@gmail.com>
2020-04-20 20:55:43 +0200

    Removed all ISOdate code and swtitched to string based dates

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,22 @@
-/*----------------------Edit transaction modal and fill the formfields with Json data-----------------------*/
+/*-------------------Open the edit transaction modal and fill the formfields with Json data-------------------*/
 
 function editTransaction(id) {
 	$.ajax({
 		url: `/edit_transaction/${id}`
 	}).done(function(response) {
     
+    //Json string to object
     let data = JSON.parse(response);
+    console.log(data)
 
+    //Grab edit fields from Json data
     document.editForm.editTransition.value = data.transition;             // Set transition radio value (on form name="editForm")
 		document.getElementById('editCategory').value = data.category_name;   // Set category value
 		document.getElementById('editDescription').value = data.details;      // Set description value
-		document.getElementById('editDate').value = data.date;                // Set date value
+    document.getElementById('editDate').value = data.date;                // Set date value
 		document.getElementById('editAmount').value = data.amount;            // Set amount value
 
+    // transaction record ID link
 		let updateURL = `/update_transaction/${id}`;
 		$('#editForm').attr('action', updateURL);
 		$('#EditTransactionModal').modal('show');
@@ -22,8 +26,12 @@ function editTransaction(id) {
 /*---------------------------Loading the libraries for the datepicker & DataTable---------------------------*/
 
 $(document).ready(function() {
-	var datepicker = flatpickr('#date', { dateFormat: 'j F, Y' });        //Datepicker settings
+  
+  //Datepicker settings
+  flatpickr('#date', { dateFormat: 'j F, Y' });
+  flatpickr('#editDate', { dateFormat: 'j F, Y' });
 
+  //DataTable settings
 	var table = $('#transactionTable').DataTable({
 		lengthChange: false,
     dom: 'lrtip',
@@ -39,6 +47,7 @@ $(document).ready(function() {
 		]
 	});
 
+  //DataTable search function
 	$('#dataTableSearch').on('keyup', function() {
 		table.search(this.value).draw();
 	});
