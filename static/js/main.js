a138fdd687f6a0ebdcc7b292f904c4925600e6d4
Djang <django.heimgartner@gmail.com>
2020-04-24 14:46:56 +0200

    Improved code readability and Json comment

--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,3 @@
-/*-------------Open the edit transaction modal and fill the formfields with Json data-------------*/
-
-function editTransaction(id) {
-  $.ajax({
-    url: `/edit_transaction/${id}`
-  }).done(function(response) {
-    
-    //Json string to an object
-    let data = JSON.parse(response);
-
-    //Grab fields from Json data
-    document.editForm.editTransition.value = data.transition;
-    document.getElementById('editCategory').value = data.category_name;
-    document.getElementById('editDescription').value = data.details;
-    document.getElementById('editDate').value = data.date;
-    document.getElementById('editAmount').value = data.amount;
-
-    // transaction record ID link
-    let updateURL = `/update_transaction/${id}`;
-    $('#editForm').attr('action', updateURL);
-    $('#editTransactionModal').modal('show');
-  });
-}
-
-/*------------------Opens the "delete transaction modal" and finds the record ID------------------*/
-
-function deleteTransaction(id) {
-  $.ajax({
-    url: `/edit_transaction/${id}`
-  }).done(function(response) {
-
-    // transaction record ID link   
-    let updateURL = `/delete_transaction/${id}`;
-    $('#deleteForm').attr('action', updateURL);
-    $('#deleteTransactionModal').modal('show');
-  });
-}
-
 /*----------------------Loading the libraries for the datepicker & DataTable----------------------*/
 
 $(document).ready(function() {
@@ -73,3 +35,41 @@ $(document).ready(function() {
     table.search(this.value).draw();
   });
 });
+
+/*-------------Open the edit transaction modal and fill the formfields with Json data-------------*/
+
+function editTransaction(id) {
+  $.ajax({
+    url: `/edit_transaction/${id}`
+  }).done(function(response) {
+    
+    //Stringify to Json JavaScript object
+    let data = JSON.parse(response);
+
+    //Grab fields from Json data
+    document.editForm.editTransition.value = data.transition;
+    document.getElementById('editCategory').value = data.category_name;
+    document.getElementById('editDescription').value = data.details;
+    document.getElementById('editDate').value = data.date;
+    document.getElementById('editAmount').value = data.amount;
+
+    // transaction record ID link
+    let updateURL = `/update_transaction/${id}`;
+    $('#editForm').attr('action', updateURL);
+    $('#editTransactionModal').modal('show');
+  });
+}
+
+/*------------------Opens the "delete transaction modal" and finds the record ID------------------*/
+
+function deleteTransaction(id) {
+  $.ajax({
+    url: `/edit_transaction/${id}`
+  }).done(function(response) {
+
+    // transaction record ID link   
+    let updateURL = `/delete_transaction/${id}`;
+    $('#deleteForm').attr('action', updateURL);
+    $('#deleteTransactionModal').modal('show');
+  });
+}
